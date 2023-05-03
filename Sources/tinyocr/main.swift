47b059e9614755f8918c201512344d840880f9cd
Richard Hristov <richardhristov@gmail.com>
2023-05-04 02:30:25 +0300

    Add support for PDFs and use the latest revision

--- Sources/tinyocr/main.swift
+++ Sources/tinyocr/main.swift
@@ -2,6 +2,7 @@ import ArgumentParser
 import Cocoa
 import Vision
 import Foundation
+import PDFKit
 
 @available(macOS 11.0, *)
 
@@ -15,7 +16,7 @@ struct tinyocr: ArgumentParser.ParsableCommand {
             completion: .file(extensions: ["txt"]))
     var words: String?
 
-    @Argument(completion: .file(extensions: ["jpeg","jpg","png","tiff"])) // support more?
+    @Argument(completion: .file(extensions: ["jpeg","jpg","png","tiff","pdf"])) // support more?
     var files: [String] = []
 
     mutating func run() throws {
@@ -29,16 +30,43 @@ struct tinyocr: ArgumentParser.ParsableCommand {
         request.recognitionLevel = VNRequestTextRecognitionLevel.accurate
         request.usesLanguageCorrection = true
         request.revision = VNRecognizeTextRequestRevision2
+        if #available(macOS 13.0, *) {
+            request.revision = VNRecognizeTextRequestRevision3
+        }
         request.recognitionLanguages = lang
         if words != nil {
             let text = try String(contentsOfFile: words!)
             request.customWords = text.components(separatedBy: "\n")
         }
         for url in files.map({ URL(fileURLWithPath: $0) }) {
-            guard let imgRef = NSImage(byReferencing: url).cgImage(forProposedRect: nil, context: nil, hints: nil) else {
-                fatalError("Error: could not convert NSImage to CGImage - '\(url)'")
+            if url.pathExtension.lowercased() == "pdf" {
+                guard let pdfDocument = PDFDocument(url: url) else {
+                    fatalError("Error: could not open PDF document - '\(url)'")
+                }
+                for pageIndex in 0..<pdfDocument.pageCount {
+                    guard let pdfPage = pdfDocument.page(at: pageIndex) else {
+                        fatalError("Error: could not get page \(pageIndex) from PDF document - '\(url)'")
+                    }
+                    let pageRect = pdfPage.bounds(for: .mediaBox)
+                    let scaleFactor: CGFloat = 4.0
+                    let thumbnailSize = CGSize(width: pageRect.width * scaleFactor, height: pageRect.height * scaleFactor)
+                    let thumbnail = pdfPage.thumbnail(of: thumbnailSize, for: .mediaBox)
+                    guard let imgRef = thumbnail.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
+                        fatalError("Error: could not convert NSImage to CGImage - '\(url)'")
+                    }
+                    let destinationURL = url.deletingPathExtension().appendingPathExtension("\(pageIndex).png")
+                    guard let destination = CGImageDestinationCreateWithURL(destinationURL as CFURL, kUTTypePNG, 1, nil) else { fatalError(
+                        "Error: could not create CGImageDestination for '\(destinationURL)'") }
+                    CGImageDestinationAddImage(destination, imgRef, nil)
+                    CGImageDestinationFinalize(destination)
+                    try? VNImageRequestHandler(cgImage: imgRef, options: [:]).perform([request])
+                }
+            } else {
+                guard let imgRef = NSImage(byReferencing: url).cgImage(forProposedRect: nil, context: nil, hints: nil) else {
+                    fatalError("Error: could not convert NSImage to CGImage - '\(url)'")
+                }
+                try? VNImageRequestHandler(cgImage: imgRef, options: [:]).perform([request])
             }
-            try? VNImageRequestHandler(cgImage: imgRef, options: [:]).perform([request])
         }
     }
 }
